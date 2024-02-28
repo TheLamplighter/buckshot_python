a087b64bfcd34f91c03cced11444e9ac2df7a74f
TheLamplighter <xtreme3010@gmail.com>
2024-02-28 10:11:54 -0300

    Basic shotgun logic

---
+++ buckshot_shotgun.py
@@ -0,0 +1,43 @@
+class BuckShot_shotgun:
+  def __init__(self, shells):
+    self.shell_arr = shells
+    self.dmg = 1
+
+  def get_damage(self) -> int:
+    return self.dmg
+
+
+  def get_shell_array(self) -> list:
+    return self.shell_arr
+  
+  def get_shell_count(self) -> int:
+    return len(self.get_shell_array())
+  
+
+  def add_shell(self, shell) -> None:
+    self.get_shell_array().append(shell)
+  
+  def eject_shell(self) -> None:
+    self.get_shell_array().pop(0)
+  
+
+  def count_shells(self, x) -> int:
+    return self.get_shell_array().count(x)
+
+  def current_bullet(self) -> int:
+    return self.get_shell_array()[0]
+
+  def fire_shotgun(self, tgt) -> None:
+    if (self.current_bullet == 1):
+      tgt.take_damage(self.get_damage())
+    self.eject_shell()
+
+
+  def calc_probability(self):
+    if self.count_shells(1) == 0:
+      return 0
+    else:
+      prob = float(
+        (self.count_shells(1)) / (self.get_shell_count())
+      )
+      return prob*100
