7d5d347c5cee550bbe0e867cd36e3b9ad6c472e3
TheLamplighter <xtreme3010@gmail.com>
2024-03-02 11:08:04 -0300

    Outlined item classes. Scalable Itemset generation done but untested.

--- buckshot_item.py
+++ buckshot_item.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
+from random import randint
+from copy import deepcopy
 
-class buckshot_item(ABC):
-
+class BuckShot_Item(ABC):
   @abstractmethod
-  def __init__(self, id) -> None:
-    self.id = id
-    pass
+  def __init__(self) -> None:
+    self.id = -1
 
   @abstractmethod
   def use():
@@ -21,16 +21,85 @@ class buckshot_item(ABC):
 # e.g. if we had a 'Comically Big Cigar' item that heals 2 instead of 1
 # the AI would know to treat them both as healing items, and not just
 # be completely clueless about how to use it.
+class Healing_Item(BuckShot_Item):
+  pass
+
+class Info_Item(BuckShot_Item):
+  pass
+
+class Damage_Item(BuckShot_Item):
+  pass
+
+class Debuff_Item(BuckShot_Item):
+  pass
+
+# Actual Item Classes Below
+class Cigarrette(Healing_Item):
+  def __init__(self):
+    self.id = 0
   
-
-class healing_item(buckshot_item):
+  def use():
+    pass
   pass
 
-class info_item(buckshot_item):
+class Handcuffs(Debuff_Item):
+  def __init__(self):
+    self.id = 1
+
+  def use():
+    pass
   pass
 
-class damage_item(buckshot_item):
+class Handsaw(Damage_Item):
+  def __init__(self):
+    self.id = 2
+
+  def use():
+    pass
   pass
 
-class debuff_item(buckshot_item):
+class Spyglass(Info_Item):
+  def __init__(self):
+    self.id = 3
+
+  def use():
+    pass
   pass
+
+class Beer(Info_Item):
+  def __init__(self):
+    self.id = 4
+
+  def use():
+    pass
+  pass
+
+
+# THIS is where all the Item IDs are stored.
+# To add a new item to the game, just make the class
+# and add the object to this list.
+# ...It would probably be smart to move this to another file later.
+item_id = list(
+    Cigarrette(), #0 
+    Handcuffs(), #1
+    Handsaw(), #2
+    Spyglass(), #3
+    Beer(), #4
+  )
+
+
+def item_by_id(id) -> object:
+  return deepcopy(item_id[id])
+
+def gen_item() -> object:
+  rng = randint(0, len(item_id))
+  return item_by_id(rng)
+
+@classmethod
+def gen_itemset(number) -> list:
+  itemset = list()
+
+  for L in range(number):
+    itemset.append(gen_item())
+
+  return itemset
